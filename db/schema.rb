9bc046c43aaea16ff0826f18e1922dbdb17eff34
lundrew <92401835+lundrew@users.noreply.github.com>
2023-04-17 20:39:54 -0700

    users scaffold created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_024408) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_031852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.bigint "conversation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
