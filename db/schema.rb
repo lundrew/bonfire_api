29e68539861cf1b355b4f49928889ccc744dc229
lundrew <92401835+lundrew@users.noreply.github.com>
2023-04-18 14:10:39 -0700

    updated schema with associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_031852) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_204452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "messages", force: :cascade do |t|
-    t.text "body"
-    t.bigint "user_id"
-    t.bigint "conversation_id"
+  create_table "conversations", force: :cascade do |t|
+    t.text "conversation_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "conversation_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_031852) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
