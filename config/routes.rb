1a2cf830fd781729d4e43dd725a188870d17eb64
lundrew <92401835+lundrew@users.noreply.github.com>
2023-05-16 13:19:00 -0700

    upd. routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  # get 'current_user', to: 'current_user#index'
-  # root to: redirect('/healthcheck', status: 302)
-  # get "/healthcheck", to: proc { [200, {}, ["success"]] }
-  # resources :conversations
-  # resources :users
-  # resources :messages
-  # devise_for :users, path: '', path_names: {
-  #   sign_in: 'login',
-  #   sign_out: 'logout',
-  #   registration: 'sign_up'
-  # },
-  # controllers: {
-  #   sessions: 'users/sessions',
-  #   registrations: 'users/registrations'
-  # } 
+  get 'current_user', to: 'current_user#index'
+  root to: redirect('/healthcheck', status: 302)
+  get "/healthcheck", to: proc { [200, {}, ["success"]] }
+  resources :conversations
   resources :users
+  resources :messages
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'sign_up'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  } 
 end
