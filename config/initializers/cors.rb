44ed3726b16a043a6c8c1e73f80dc0fb7f717052
lundrew <92401835+lundrew@users.noreply.github.com>
2023-05-16 15:54:51 -0700

    edit cors origins url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://bonfire-chat-production.up.railway.app/'
+    origins 'https://bonfire-chat-production.up.railway.app'
     resource(
       '*',
       headers: :any,
