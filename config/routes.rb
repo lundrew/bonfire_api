8ea0fb019460ba793477e0d0b417209763fd2a61
lundrew <92401835+lundrew@users.noreply.github.com>
2023-05-16 12:44:27 -0700

    users test

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
-  get 'current_user', to: 'current_user#index'
-  root to: redirect('/healthcheck', status: 302)
-  get "/healthcheck", to: proc { [200, {}, ["success"]] }
-  resources :conversations
+  # get 'current_user', to: 'current_user#index'
+  # root to: redirect('/healthcheck', status: 302)
+  # get "/healthcheck", to: proc { [200, {}, ["success"]] }
+  # resources :conversations
+  # resources :users
+  # resources :messages
+  # devise_for :users, path: '', path_names: {
+  #   sign_in: 'login',
+  #   sign_out: 'logout',
+  #   registration: 'sign_up'
+  # },
+  # controllers: {
+  #   sessions: 'users/sessions',
+  #   registrations: 'users/registrations'
+  # } 
   resources :users
-  resources :messages
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'sign_up'
-  },
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
